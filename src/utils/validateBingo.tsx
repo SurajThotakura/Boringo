335e57ebd9ccdb5b5273d856535e0a4a447bda0c
Suraj Thotakura <31972379+SurajThotakura@users.noreply.github.com>
2023-11-28 09:51:46 +0530

    feat: optimize validation logic

--- src/utils/validateBingo.tsx
+++ src/utils/validateBingo.tsx
@@ -1,39 +1,28 @@
 export function validateBingoCard(cardString: string, n: number) {
   const validString = "1".repeat(n);
 
-  // check rows
+  // check rows, columns and diagonals
   const rowsArray = []
+  const columnsArray = []
+  let rightDiag = "";
+  let leftDiag = "";
   for(let x=0; x<n; x++){
     let row = ""
+    let column = ""
+    leftDiag = leftDiag + cardString[x * (n + 1)];
+    rightDiag = rightDiag + cardString[n - 1 + x * (n - 1)];
     for(let y=0; y<n; y++){
         row = row + cardString[x*n+y]
+        column = column + cardString[x+n*y]
     }
     rowsArray[x] = row
+    columnsArray[x] = column
   }
   if(rowsArray.includes(validString)){
     return true
   }
-
-  // check columns
-  const columnsArray = []
-  for(let x=0; x<n; x++){
-    let column = ""
-    for(let y=0; y<n; y++){
-        column = column + cardString[x+n*y]
-    }
-    columnsArray[x] = column
-  }
   if(columnsArray.includes(validString)){
     return true
-  }
-
-  // check diagonals
-
-  let rightDiag = "";
-  let leftDiag = "";
-  for (let x = 0; x < n; x++) {
-    leftDiag = leftDiag + cardString[x * (n + 1)];
-    rightDiag = rightDiag + cardString[n - 1 + x * (n - 1)];
   }
   if (leftDiag === validString) {
     return true;
