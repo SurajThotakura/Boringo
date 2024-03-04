33026d073ee5aada24e34d06ca752443dc51df61
Suraj Thotakura <31972379+SurajThotakura@users.noreply.github.com>
2024-03-04 20:58:35 +0530

    feat: BREAKING update colors to tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import { colors } from './src/constants/colors'
+
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
+		colors: colors,
+		fontFamily: {
+			sans: ['IBM Plex Mono', 'sans-serif'],
+		},
 		extend: {},
 	},
 	plugins: [],
